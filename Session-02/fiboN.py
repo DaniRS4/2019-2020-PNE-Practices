2db02d88b92329195228fc44c825eb6345f27b43
dreal <d.real.2019@alumnos.urjc.es>
2020-02-04 16:38:01 +0100

    Fibonacci funtion

--- Session-02/fiboN.py
+++ Session-02/fiboN.py
@@ -1,13 +1,16 @@
 # Fibonacci funtion
 
 
-def fibon (n):
+def fibon(n):
     a = 0
     b = 1
-    for i in range(n+1):
+    for i in range(n):
         c = b + a
         a = b
         b = c
-        return(a)
+    return a
 
-print(fibon(5))
+
+print("5th fibonacci term:", fibon(5))
+print("10th fibonacci term:", fibon(10))
+print("15th fibonacci term:", fibon(15))
