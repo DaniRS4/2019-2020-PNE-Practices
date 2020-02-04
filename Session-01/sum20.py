22b4849fb05260052423122a4e1f8a42defaaa9e
dreal <d.real.2019@alumnos.urjc.es>
2020-02-04 15:39:10 +0100

    just another test...

---
+++ Session-01/sum20.py
@@ -0,0 +1,12 @@
+# Session 1. Exercise 3
+# Calculate the sum of the 20 first interger numbers
+# 1 + 2 + 3 + ... + 20
+# Example working!
+
+# -- Store the result
+res = 0
+
+for i in range (1, 21):
+    res += 1
+
+print("The total sum is: ", res)
