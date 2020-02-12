7c8d71fc17f50f3554de8a77ebc5323962bba933
dreal <d.real.2019@alumnos.urjc.es>
2020-02-12 18:06:36 +0100

    A

--- Session-04 folder/sequence.py
+++ Session-04 folder/sequence.py
@@ -7,6 +7,6 @@ file_contents = Path(FILENAME).read_text()
 body = file_contents.split("\n")[1:]
 nucleotids = " "
 nucleotids = nucleotids.join(body)
-nucleotids = nucleotids.replace(" ","")
+nucleotids = nucleotids.replace(" ", "")
 
 print(len(nucleotids))
