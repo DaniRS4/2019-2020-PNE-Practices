c4e8516c263464c76f0736d8f4b9b5d425d3611f
dreal <d.real.2019@alumnos.urjc.es>
2020-02-19 17:18:08 +0100

    Session 5

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -1,9 +1,7 @@
-from pathlib import Path
-
-
 def seq_ping():
     print("OK!")
 
+from pathlib import Path
 
 def seq_read_fasta(filename):
     bodystr = ""
@@ -11,4 +9,82 @@ def seq_read_fasta(filename):
     lines = file_contents.split('\n')
     body = lines[1:]
     bodystr = bodystr.join(body).replace(" ", "")
-    return bodystr[0:20]
+    return (bodystr[0:20])
+
+def seq_len(seq):
+    file_contents = Path(seq).read_text()
+    bodystr = ""
+    lines = file_contents.split('\n')
+    body = lines[1:]
+    bodystr = bodystr.join(body).replace(" ", "")
+    return (len(bodystr))
+
+def seq_count_base(seq, base):
+    count = 0
+    file_contents = Path(seq).read_text()
+    seq = ""
+    lines = file_contents.split('\n')
+    body = lines[1:]
+    seq = seq.join(body).replace(" ", "")
+    for i in seq:
+        if i == base:
+            count += 1
+    return (count)
+
+def seq_count(seq):
+    FOLDER = "/home/alumnos/joseepp/PycharmProjects/2019-2020-PNE-Practices/Session-04/"
+    FILENAME = ["U5", "ADA", "FRAT1", "FXN", "U5"]
+    BASES = ["A", "T", "C", "G"]
+    file_contents = Path(seq).read_text()
+    seq = ""
+    lines = file_contents.split('\n')
+    body = lines[1:]
+    seq = seq.join(body).replace(" ", "")
+    countA = 0
+    countT = 0
+    countC = 0
+    countG = 0
+    countlist = []
+    for i in seq:
+        if i == "A":
+            countA = countA + 1
+        elif i == "T":
+            countT = countT + 1
+        elif i == "C":
+            countC = countC + 1
+        elif i == "G":
+            countG = countG + 1
+        else:
+            next
+    countlist.append(countA)
+    countlist.append(countT)
+    countlist.append(countC)
+    countlist.append(countG)
+    for i in countlist:
+        basesdict = dict(zip(BASES, countlist))
+    return (basesdict)
+
+def seq_reverse(filename):
+    bodystr = ""
+    file_contents = Path(filename).read_text()
+    lines = file_contents.split('\n')
+    body = lines[1:]
+    bodystr = bodystr.join(body).replace(" ", "")
+    first20 = bodystr[0:20]
+    first20reverb = first20[::-1]
+    return (first20, first20reverb)
+
+def seq_complement(seq):
+    comp = ""
+    for i in seq:
+        if i == "A":
+            comp = comp + "T"
+        elif i == "C":
+            comp = comp + "G"
+        elif i == "G":
+            comp = comp + "C"
+        elif i == "T":
+            comp = comp + "A"
+        else:
+            next
+    return comp
