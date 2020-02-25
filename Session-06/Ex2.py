d701de6bd11a38f9fb6a0d669adc8161dbe440a1
DANIEL REAL <d.real.2019@alumnos.urjc.es>
2020-02-26 00:25:17 +0100

    Session - 06

---
+++ Session-06/Ex2.py
@@ -0,0 +1,20 @@
+class Seq:
+    def __init__(self, strbases):
+        self.strbases = strbases
+        print("New sequence created!")
+    def __str__(self):
+        return self.strbases
+    def len(self):
+        return len(self.strbases)
+    pass
+
+def print_seq(seq_list):
+    for element in seq_list:
+        seq = seq_list[seq_list.index(element)]
+        print(f"Sequence: {seq_list.index(element)} (Length: {seq.len()}) {seq}")
+
+# --- Main program
+
+seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
+
+print_seq(seq_list)
