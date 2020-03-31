e713f2e11925eab45771e8bd29303f1d845cef2a
DANIEL REAL <d.real.2019@alumnos.urjc.es>
2020-03-31 16:14:14 +0200

    Session 10

---
+++ Session-10/client-test.py
@@ -0,0 +1,11 @@
+from Client0 import Client
+
+IP = "212.128.253.128"
+PORT = 8080
+c = Client(IP, PORT)
+talk = 'Message'
+
+for e in range(5):
+    talk += str(e)
+    c.debug_talk(talk)
+    talk = "Message"
---
+++ Session-10/happy-server.py
@@ -0,0 +1,47 @@
+import socket
+
+IP = "212.128.253.128"
+PORT = 8080
+# -- Step 1: Create a socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Step 2: Bind socket to server's IP adress and port
+ls.bind((IP, PORT))
+
+# -- Step 3: Convert into a listening socket
+ls.listen()
+
+print("El server está activado")
+
+while True:
+
+    # -- Wait for client to connect
+    print("Waiting for Clients to connect")
+    try:
+        (cs, client_ip_port) = ls.accept()
+
+        print("A client has connected to the server!")
+    except KeyboardInterrupt:
+        print("hola que tal?")
+        ls.close
+        exit()
+
+    # -- Read the message from the client
+    # -- The received message is in raw bytes
+    msg_raw = cs.recv(2048)
+
+    # -- We decode it for converting it
+    # -- into a human-redeable string
+    msg = msg_raw.decode()
+
+    # -- Print the received message
+    print(f"Received Message: {msg}")
+
+    response = ("Makinaaa\n")
+    cs.send(response.encode())
+
+
+    # -- Close the socket
+
+
+    cs.close()
